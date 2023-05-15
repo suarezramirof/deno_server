fd41bd02d7b47b9dadca4d410b77822b4dcb420e
Ramiro Suarez <suarezramirof@gmail.com>
2023-05-15 19:10:29 -0300

    refactor: use express

--- server.tsx
+++ server.tsx
@@ -1,55 +1,66 @@
-import React, { ChangeEvent } from "https://esm.sh/react@17.0.2";
-import ReactDOMServer from "https://esm.sh/react-dom@17.0.2/server";
+import express, {Request, Response} from "npm:express@4.18.2";
+import React from "https://esm.sh/react@17.0.2";
+import { renderToString } from "https://esm.sh/react-dom@17.0.2/server";
 
-import {
-  Application,
-  opine,
-  Request,
-  Response,
-  targetValue
-} from "https://deno.land/x/opine@2.3.4/mod.ts";
-
-const app: Application = opine();
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.listen(
-  3000,
-  () => console.log("server has started on http://localhost:3000 🚀"),
-);
-
-const html = `
-<html>
-  <head>
-    <style>body {background-color: black;}</style>
-  </head>
-  <body>
-    <div id="root">${appString}</div>
-  </body>
-</html>
-`;
+const app = express();
+app.use(express.urlencoded({extended: false}))
+app.use(express.json());
 
 const ColorForm = () => {
-  const [color, setColor] = React.useState("");
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const response = await fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ color }),
-    });
-    setColor("");
-  };
-  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setColor(targetValue(e));
-  };
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={color} onChange={handleOnChange} />
+      <form action="/" method="post">
+        <input
+          type="text"
+          placeholder="Enter a color"
+          name="col"
+          id="col"
+        />
+        <button>Submit</button>
       </form>
     </div>
   );
 };
+
+const ColorList = ({ colors }: { colors: string[] }) => (
+  <ul>
+    {colors.map((color, index) => (
+      <li key={index} style={{ color }}>
+        {color}
+      </li>
+    ))}
+  </ul>
+);
+
+const colors: string[] = [];
+
+app.get("/", (req: Request , res: Response) => {
+  const appString = renderToString(
+    <>
+      <ColorForm />
+      <ColorList colors={colors} />
+    </>,
+  );
+
+  res.send(`
+    <html>
+      <head>
+        <style>body { background-color: black; }</style>
+      </head>
+      <body>
+        <div id="root">${appString}</div>
+      </body>
+    </html>
+  `);
+});
+
+app.post("/", (req: Request, res: Response) => {
+  const color = req.body.col;
+  console.log(color);
+  colors.push(color || "");
+  res.redirect("/");
+});
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
